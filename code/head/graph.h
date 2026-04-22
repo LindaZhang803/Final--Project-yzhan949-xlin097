b743e7b2436eade19f00902379b1611122888f43
Lucyyy127 <xlin1212lucy@gmail.com>
2026-04-21 17:18:01 -0700

    add definitions for new methods

--- code/head/graph.h
+++ code/head/graph.h
@@ -8,6 +8,10 @@ struct CSRGraph {
     int num_vertices = 0;
     std::vector<int> offsets;
     std::vector<int> edges;
+    std::vector<int> weights;
+    std::vector<int> in_offsets;
+    std::vector<int> in_edges;
+    std::vector<int> in_weights;
 };
 
 CSRGraph LoadGraph(const std::string& filename);
